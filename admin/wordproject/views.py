e4a42f292c252df3a6586301a4afac64f4c3276a
iiSABRE <cameronthomasjackson@gmail.com>
2016-08-01 14:10:02 +1200

    .

---
+++ admin/wordproject/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.core import serializers
+from  django.http import HttpResponse
+from wordproject.models import WordRecord
+
+
+def word_json(request):
+    words = WordRecord.objects.all()
+    data = serializers.serialize("json", words)
+    return HttpResponse(data, content_type='application/json')
+	
+def word_json_update(request):
+    words = WordRecord.objects.all()
+    data = serializers.serialize("json", words)
+    return HttpResponse(data, content_type='application/json')
