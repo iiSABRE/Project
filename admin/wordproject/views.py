78ff8be0e43fefde5654b537394a047f7fb0f7af
iiSABRE <cameronthomasjackson@gmail.com>
2016-08-02 12:06:18 +1200

    .

--- admin/wordproject/views.py
+++ admin/wordproject/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.core import serializers
 from  django.http import HttpResponse
+from django.utils import timezone
+from django.utils import timezone
 from wordproject.models import WordRecord
 
 
@@ -11,6 +13,6 @@ def word_json(request):
     return HttpResponse(data, content_type='application/json')
 	
 def word_json_update(request):
-    words = WordRecord.objects.all()
+    words = WordRecord.objects.filter(dateUpdated__gte=datetime(2016, 7, 25))
     data = serializers.serialize("json", words)
     return HttpResponse(data, content_type='application/json')
