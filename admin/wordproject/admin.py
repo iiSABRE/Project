f2e274b6664e8ad967d3e57f8e5c7b8d695cd562
iiSABRE <cameronthomasjackson@gmail.com>
2016-09-13 14:12:41 +1200

    h

--- admin/wordproject/admin.py
+++ admin/wordproject/admin.py
@@ -18,7 +18,7 @@ class WordRecordAdmin(admin.ModelAdmin):
     def get_secondary(self, obj):
          b = WordPair.objects.filter(original=obj.id).values('translation')
          c = WordRecord.objects.filter(id=b)		 
-         return c
+         return c.WordRecord
 
 
 class SoundAdmin(admin.ModelAdmin):
