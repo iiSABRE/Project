a47d1b4679b54933d8db17b1ef14247d7fd1526d
iiSABRE <cameronthomasjackson@gmail.com>
2016-09-13 15:30:00 +1200

    j

--- admin/wordproject/admin.py
+++ admin/wordproject/admin.py
@@ -19,7 +19,7 @@ class WordRecordAdmin(admin.ModelAdmin):
          b = WordPair.objects.filter(original=obj.id).values('translation')
          c = WordRecord.objects.filter(id=b)
          if c:
-             for i, c in enumerate(queryset):		 
+             for i, c in enumerate(c):		 
                return c[i].language
          else:
                return "-"	
