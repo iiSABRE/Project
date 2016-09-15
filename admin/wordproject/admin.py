7611f13ee2e13719cf017e3fbc916bdd3c83c717
iiSABRE <cameronthomasjackson@gmail.com>
2016-09-15 15:48:22 +1200

    h

--- admin/wordproject/admin.py
+++ admin/wordproject/admin.py
@@ -21,7 +21,7 @@ class WordRecordAdmin(admin.ModelAdmin):
          #c = WordRecord.objects.filter(id=b.original)
          if b:
             for wordpair in b:
-               return linebreaks(wordpair.translation)
+               return ' %s' % (wordpair.translation)
          else:
                return "-"
     Translation_Language.allow_tags = True
